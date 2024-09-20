20718845a9b81fe7d499468915c2cbe84fbaa7cc
g-getchaisrie <getchaisrie.gm@gmail.com>
2024-09-20 23:27:59 +0700

    new1

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 export default function App() {
   const [level, setLevel] = useState(0);
   const [imageIndex, setImageIndex] = useState(0);
-  const images = ["public/images/340273.webp", "public/images/279591131_117538467604669_8217254012377704025_n.jpg"];
-  const buttonImages = ["public/images/1122b81e714da8a4fc2ffc44c5488f5f.jpg", "public/images/ai-generated-8241575_1280.webp", "public/images/istockphoto-1440269387-170667a.jpg"];
+  const images = ["https://th.bing.com/th/id/R.96e9dc89447ac28dce372f10a49aa234?rik=eWn2AmoBJVdY5g&pid=ImgRaw&r=0", "https://scontent.fbkk33-2.fna.fbcdn.net/v/t39.30808-6/279591131_117538467604669_8217254012377704025_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHismYOFMYvE36X1FdzTc4DB3HkX5EPVuUHceRfkQ9W5YehQO8rT7OLeERV9Yo-5Fsvz3d6fRyUjvloIMbRzMlH&_nc_ohc=gbUXy-QiAPcQ7kNvgGE267H&_nc_zt=23&_nc_ht=scontent.fbkk33-2.fna&_nc_gid=AfhKhGNO06zUM4VvX63XZy8&oh=00_AYByhjbOutDBakwbNL3pR_NbDYovdvZZzE-wJkZatCyFvA&oe=66F363C8"];
+  const buttonImages = ["https://th.bing.com/th/id/OIP.u7aCAcj6IHRXoPHxYvc-uAHaG7?w=800&h=749&rs=1&pid=ImgDetMain", "https://png.pngtree.com/png-clipart/20230914/original/pngtree-cute-cartoon-pumpkin-sticker-vector-illustration-png-image_12164530.png", "https://tse4.mm.bing.net/th?id=OIP.kRgX9fYomRiWwRnw0vlGfQHaFj&pid=Api&P=0&h=180"];
 
   const increaseLevel = (increment) => {
     setLevel((prevLevel) => {
@@ -106,6 +106,21 @@ export default function App() {
             <p onClick={() => increaseLevel(20)} style={buttonText}>
               Level +20
             </p>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <img
+              src={buttonImages[2]}
+              alt="button"
+              style={buttonImageStyle}
+              onClick={() => increaseLevel(30)}
+              onMouseDown={(e) => (e.currentTarget.style.transform = "scale(0.9)")}
+              onMouseUp={(e) => (e.currentTarget.style.transform = "scale(1)")}
+            />
+            <p onClick={() => increaseLevel(30)} style={buttonText}>
+              Level +30
+            </p>
+          </div>
+          <div style={{ textAlign: "center" }}>
           </div>
         </div>
       </div>
